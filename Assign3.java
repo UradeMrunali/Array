fbcab475f04a3a2d64b000ddddbe91dc8d7c73f0
Mrunali <mrunaliurade26@gmail.com>
2023-03-24 16:45:46 +0530

    test if an array contains a specific value

---
+++ Assign3.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Assign3 {
+ public static void main(String[] args){
+	 int a[]= {45,14,23,6,77};
+	 int num=14;
+	
+	
+	 
+	 for(int i=0;i<a.length;i++){
+		 
+	 if(num==a[i]){
+		 System.out.println("array contains this value :"+ num);
+	 
+ }
+}
+}
+}
+
+ /* int b[]= new int[4];
+  int i;
+  int c;
+  
+  Scanner sc=new Scanner(System.in);
+  
+  for( i=0;i<b.length;i++){
+	  System.out.println("enter numbers :" +(i+1));
+	  b[i]=sc.nextInt();
+  }
+  System.out.print(" numbers you entered :");
+  for( i=0;i<b.length;i++){
+	  System.out.print(b[i]+" ");
+  }
+  System.out.println();
+  System.out.println("Enter number you want to search: ");
+  c=sc.nextInt();
+  for(i=0;i<c;i++){
+  if(b[i]==c){
+	 
+	  System.out.println("The number you are searching is available :"+c);
+  }
+ else {
+	 System.out.println("The number is not available");
+  }
+  
+  }
+ }
+}*/
+
+
+  
+  
+
+ 
