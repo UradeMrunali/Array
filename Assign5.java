e168e551af26cef04286b210145787a47fdd0653
Mrunali <mrunaliurade26@gmail.com>
2023-03-26 19:35:07 +0530

    find the max and min value of array

---
+++ Assign5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+//incomplete
+public class Assign5 {
+	public static void main(String args[]){
+		int a[]={4,5,2,14,8,12};
+		int min_element=0;
+		int max_element=0;
+		
+		Scanner sc = new Scanner(System.in);
+		for(int i=0;i<a.length;i++){
+			System.out.print(a[i]+" ");
+//			if(min_element<a[i]){
+//				
+//				System.out.println(min_element);
+//			}
+//			else if(max_element>a[i]){
+//				System.out.println(max_element);
+//				
+//			}
+			
+			
+			
+		}
+		
+		
+		
+	}
+  
+}
