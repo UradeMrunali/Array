860d2db77e97b51334a0045779a9d2f623c8ddd8
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2023-04-01 22:21:05 +0530

    Update Sumof2arr.java

--- Sumof2arr.java
+++ Sumof2arr.java
@@ -16,31 +16,33 @@ public class Sumof2arr
 		 { 
 			 System.out.println("Enter int numbers for 1st array :"+ i+" "+j); 
 			 a1[i][j]=Integer.parseInt(sc.nextLine());
-		
-		
 		 }
 		 }
 	 
-	 for( int i=0;i<a2.length;i++){
-		 for( int j=0;j<a2.length;j++){
-			
-			 
-			 System.out.println("Enter int numbers for 2nd array :"+ i+" "+j); 
+	 for( int i=0;i<a2.length;i++)
+	 {
+		 for( int j=0;j<a2.length;j++)
+		 { 
+		   System.out.println("Enter int numbers for 2nd array :"+ i+" "+j); 
 			 a2[i][j]=sc.nextInt();
 			 
 
 		 }
 		 		 }
-	 for( int i=0;i<a3.length;i++){
-		 for( int j=0;j<a3.length;j++){
+	 for( int i=0;i<a3.length;i++)
+	 {
+		 for( int j=0;j<a3.length;j++)
+		 {
 			 a3[i][j]=( a1[i][j] + a2[i][j]) ;
         
 		 }
-		 		 }
+		  }
 	 
 	 System.out.println("1st matrix:");
-	 	 for( int i=0;i<a1.length;i++){
-		 for( int j=0;j<a1.length;j++){
+	 	 for( int i=0;i<a1.length;i++)
+		 {
+		 for( int j=0;j<a1.length;j++)
+		 {
 			
 		 	
 			 System.out.print(a1[i][j]+ " "); 
@@ -51,20 +53,21 @@ public class Sumof2arr
 	
 	 System.out.println("2nd matrix:");
 	
-	 for( int i=0;i<a2.length;i++){
-		 for( int j=0;j<a2.length;j++){
-			
-		 	
+	 for( int i=0;i<a2.length;i++)
+	 {
+		 for( int j=0;j<a2.length;j++)
+		 {
 			 System.out.print(a2[i][j]+ " ");
 		 }
 		 System.out.println();
 		 
 		 }
 	 System.out.println("sum of these matrix is:");
-	 for( int i=0;i<a3.length;i++){
-		 for( int j=0;j<a3.length;j++){
-			
-			 System.out.print(a3[i][j]+" ");
+	 for( int i=0;i<a3.length;i++)
+	 {
+		 for( int j=0;j<a3.length;j++)
+		 {
+		   System.out.print(a3[i][j]+" ");
 			 }
          System.out.println();
 	 }
